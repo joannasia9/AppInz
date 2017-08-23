fed484f7a8efe5defb87f1f74d9ba625f7538655
Joanna Maciak <joannasia.maciak@gmail.com>
2017-08-23 15:36:27 +0200

    Main menu items created.

--- app/src/main/java/com/example/asia/jmpro/MainMenu.java
+++ app/src/main/java/com/example/asia/jmpro/MainMenu.java
@@ -1,13 +1,26 @@
 package com.example.asia.jmpro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainMenu extends AppCompatActivity {
+    ListView mItems;
+    String[] mItemsTitles;
+    int[] mItemsBackground = {R.color.item1,R.color.item2,R.color.item3,R.color.item4};
+    int[] mItemsImages = {R.drawable.item1,R.drawable.item2,R.drawable.item3, R.drawable.item4};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mItems = (ListView) findViewById(R.id.menuItemsListView);
+        mItemsTitles = getResources().getStringArray(R.array.main_menu_items);
+
+        MyMenuAdapter myMenuAdapter = new MyMenuAdapter(this,mItemsTitles,mItemsImages,mItemsBackground);
+        mItems.setAdapter(myMenuAdapter);
     }
 }
