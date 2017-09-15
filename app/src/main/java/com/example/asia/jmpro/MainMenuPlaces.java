aaed23165d5c110c1fe72578bcc61bc366439143
Joanna Maciak <joannasia.maciak@gmail.com>
2017-09-15 22:04:54 +0200

    Current localization verified.

--- app/src/main/java/com/example/asia/jmpro/MainMenuPlaces.java
+++ app/src/main/java/com/example/asia/jmpro/MainMenuPlaces.java
@@ -1,10 +1,17 @@
 package com.example.asia.jmpro;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -12,6 +19,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -19,7 +27,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,7 +42,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class MainMenuPlaces extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
     DrawerLayout drawer;
     TextView optionsTitle;
     String[] placesOptions;
@@ -70,7 +86,6 @@ public class MainMenuPlaces extends AppCompatActivity
 
         selectItem(0);
 
-        ////Maps Operations
         initMap();
 
     }
@@ -170,25 +185,47 @@ public class MainMenuPlaces extends AppCompatActivity
         }
     }
 
-    private void getCurrentLocation() {
-
-    }
-
     private void showSuggestedPlaces() {
-
+        // 1.Get locations from database
+        // 2. Show markers
     }
 
     private void addSuggestedPlace() {
-
+        // 1. Get current coordinates or location / addres (?)
+        // 2. Save place to database
     }
 
+    private void getCurrentCoordinates() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, false);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        double currentLongitude = location.getLongitude();
+        double currentLatitude = location.getLatitude();
+
+        Toast.makeText(getApplicationContext(), "Lat: " + currentLatitude + "Long: " + currentLongitude, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        goToLocationZoom(52.156034, 21.034499, 8);
+        goToLocationZoom(52.23, 21.01, 8);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -200,7 +237,74 @@ public class MainMenuPlaces extends AppCompatActivity
             return;
         }
         map.setMyLocationEnabled(true);
+        map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                checkIfLocalizationEnabled();
+                setGoogleApiClient();
+
+                getCurrentCoordinates();
+                return false;
+            }
+        });
     }
 
+    private void setGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        googleApiClient.connect();
+    }
+
+    private void checkIfLocalizationEnabled() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.gps_off);
+            builder.setMessage(R.string.wanna_gps_on);
+            builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                }
+            });
+            builder.setNegativeButton(R.string.no, null);
+            builder.create().show();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //location request:::
+        //        locationRequest=LocationRequest.create();
+        //        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+        //        locationRequest.setInterval(1000); //miliseconds
+        //        locationServices.FocusedLocationAPI.requestLocationUpdates(googleApiClient,locationRequest,this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            Toast.makeText(this, R.string.cant_get_current_location, Toast.LENGTH_LONG).show();
+        } else {
+
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+            map.animateCamera(update);
+        }
+    }
 }
 
